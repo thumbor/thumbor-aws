6815844df014e1466ed94cb073e801e3a55eb69f
Aymeric Augustin <aymeric.augustin@fractalideas.com>
2022-12-04 13:36:56 +0100

    Convert Thumbor URLs to S3 paths consistently.
    Fix #66.

---
+++ thumbor_aws/utils.py
@@ -0,0 +1,13 @@
+from urllib.parse import unquote
+
+def normalize_path(prefix: str, path: str) -> str:
+    """Convert a URL received from Thumbor to a key in a S3 bucket."""
+    # Thumbor calls load/store functions with a URL that is  URL-encoded and
+    # that always starts with a slash. S3Client doesn't expect URL-encoding
+    # and S3 keys don't usually start with a slash. Decode and remove slash.
+    path = unquote(path).lstrip("/")
+    if prefix:
+        # Avoid double slash if prefix ends with a slash.
+        prefix = prefix.rstrip("/")
+        return f"{prefix}/{path}"
+    return path
