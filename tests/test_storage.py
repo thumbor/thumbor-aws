4c8cf4b39bdb6f384bc8ad859429e6ef4ca6be77
Peter Schrammel <peter.schrammel@gmx.de>
2024-06-27 18:34:41 +0000

    false thy python style

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -146,7 +146,7 @@ class StorageTestCase(BaseS3TestCase):
             b"ACME-SEC2",
             "application/text",
             "http://my-site.com",
-            false
+            False
         )
 
         data = await storage.get_crypto(filepath)
@@ -164,7 +164,7 @@ class StorageTestCase(BaseS3TestCase):
             b'{"some": "data"}',
             "application/text",
             "",
-            false
+            False
         )
 
         data = await storage.get_detector_data(filepath)
